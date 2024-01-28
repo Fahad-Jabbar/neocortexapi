568042afd35f3f4e91edf1d3fa8c25868faae778
kashif1294 <kashifhu@dlmu.edu.cn>
2024-01-28 15:32:27 +0100

    Adding KNN class with basic implementation

--- source/NeoCortexApi/Classifiers/KNN_CQ.cs
+++ source/NeoCortexApi/Classifiers/KNN_CQ.cs
@@ -6,5 +6,15 @@ namespace NeoCortexApi.Classifiers
 {
     internal class KNN_CQ
     {
+        private List<double[]> trainingData;
+        private List<int> labels;
+        private int k;
+
+        public KNNClassifier(int k)
+        {
+            this.k = k;
+            trainingData = new List<double[]>();
+            labels = new List<int>();
+        }
     }
 }
