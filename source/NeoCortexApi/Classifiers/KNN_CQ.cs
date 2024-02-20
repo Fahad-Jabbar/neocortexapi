883a5f2b5f8147b65b8e34dfdefb090242d016d5
kashif1294 <kashifhu@dlmu.edu.cn>
2024-02-20 01:15:14 +0100

    modified PredictSingleInstance to calculate distance between new and existing data points

--- source/NeoCortexApi/Classifiers/KNN_CQ.cs
+++ source/NeoCortexApi/Classifiers/KNN_CQ.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace NeoCortexApi.Classifiers
 {
-    internal class KNN_CQ
+    public class KNN_CQ
     {
         private List<double[]> trainingData;
         private List<int> labels;
@@ -16,13 +16,14 @@ namespace NeoCortexApi.Classifiers
             trainingData = new List<double[]>();
             labels = new List<int>();
         }
+
         public void Train(List<double[]> data, List<int> targetLabels)
-            {
+        {
             if (data.Count != targetLabels.Count)
                 throw new ArgumentException("Number of data points must be equal to the number of labels.");
 
-              trainingData = data;
-              labels = targetLabels;
+            trainingData = data;
+            labels = targetLabels;
         }
 
         private double CalculateDistance(double[] point1, double[] point2)
@@ -30,15 +31,7 @@ namespace NeoCortexApi.Classifiers
             if (point1.Length != point2.Length)
                 throw new ArgumentException("Data points must have the same number of dimensions.");
 
-            double sumOfSquares = 0.0;
-
-            for (int i = 0; i < point1.Length; i++)
-            {
-                double diff = point1[i] - point2[i];
-                sumOfSquares += diff * diff;
-            }
-
-            return Math.Sqrt(sumOfSquares);
+            return Math.Sqrt(point1.Zip(point2, (x, y) => Math.Pow(x - y, 2)).Sum());
         }
 
         public List<int> Predict(List<double[]> newData)
@@ -46,70 +39,20 @@ namespace NeoCortexApi.Classifiers
             if (trainingData.Count == 0)
                 throw new InvalidOperationException("The classifier has not been trained yet.");
 
-            List<int> predictedLabels = new List<int>();
-
-            foreach (var dataPoint in newData)
-            {
-                int predictedLabel = PredictSingleInstance(dataPoint);
-                predictedLabels.Add(predictedLabel);
-            }
-
-            return predictedLabels;
+            return newData.Select(PredictSingleInstance).ToList();
         }
-
-        // Adding a method for distance sorting
 
         private int PredictSingleInstance(double[] newDataPoint)
         {
-            // Calculate distances from newDataPoint to all training data points
-            List<Tuple<double, int>> distancesAndLabels = new List<Tuple<double, int>>();
+            var distancesAndLabels = trainingData.Select((dataPoint, i) => new { Distance = CalculateDistance(newDataPoint, dataPoint), Label = labels[i] })
+                                                 .OrderBy(x => x.Distance)
+                                                 .Take(k)
+                                                 .ToList();
 
-            for (int i = 0; i < trainingData.Count; i++)
-            {
-                double distance = CalculateDistance(newDataPoint, trainingData[i]);
-                distancesAndLabels.Add(new Tuple<double, int>(distance, labels[i]));
-            }
-
-            // Sort the distancesAndLabels list based on distances in ascending order
-            distancesAndLabels.Sort((x, y) => x.Item1.CompareTo(y.Item1));
-
-            // Take the first k elements from the sorted list
-            List<int> kNearestLabels = new List<int>();
-
-            for (int i = 0; i < k; i++)
-            {
-                kNearestLabels.Add(distancesAndLabels[i].Item2);
-            }
-
-            // Perform a majority voting to determine the predicted label
-            int predictedLabel = MajorityVoting(kNearestLabels);
-
-            return predictedLabel;
+            return distancesAndLabels.GroupBy(x => x.Label)
+                                     .OrderByDescending(x => x.Count())
+                                     .First()
+                                     .Key;
         }
-
-        private int MajorityVoting(List<int> labels)
-        {
-            Dictionary<int, int> labelCount = new Dictionary<int, int>();
-
-            foreach (var label in labels)
-            {
-                if (labelCount.ContainsKey(label))
-                    labelCount[label]++;
-                else
-                    labelCount[label] = 1;
-            }
-
-        }
+    }
 }
-    // Add a counting method
-    private int MajorityVoting(List<int> labels)
-    {
-        Dictionary<int, int> labelCount = new Dictionary<int, int>();
-
-        foreach (var label in labels)
-        {
-            if (labelCount.ContainsKey(label))
-                labelCount[label]++;
-            else
-                labelCount[label] = 1;
-        }
