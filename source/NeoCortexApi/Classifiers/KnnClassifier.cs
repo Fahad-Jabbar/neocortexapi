bf049a5abddf536d5f6ed0a0cc07c9ee59130755
kashif1294 <kashifhu@dlmu.edu.cn>
2024-03-17 03:17:27 +0100

    Modified KNNclassifier

--- source/NeoCortexApi/Classifiers/KnnClassifier.cs
+++ source/NeoCortexApi/Classifiers/KnnClassifier.cs
@@ -1,19 +1,16 @@
-﻿using System;
+﻿using NeoCortexApi.Entities;
+using System;
 using System.Collections.Generic;
-using NeoCortexApi.Entities;
 using System.Linq;
-using NeoCortexApi.Classifiers;
 
 namespace NeoCortexApi.Classifiers
 {
-    // Extension method for IEnumerable to get item and index
     public static class EnumExtension
     {
         public static IEnumerable<(T item, int index)> WithIndex<T>(this IEnumerable<T> self)
             => self.Select((item, index) => (item, index));
     }
 
-    // DefaultDictionary implementation
     public class DefaultDictionary<TKey, TValue> : Dictionary<TKey, TValue> where TValue : new()
     {
         public new TValue this[TKey key]
@@ -32,7 +29,6 @@ namespace NeoCortexApi.Classifiers
         }
     }
 
-    // ClassificationAndDistance class
     public class ClassificationAndDistance : IComparable<ClassificationAndDistance>
     {
         public string Classification { get; }
@@ -49,68 +45,42 @@ namespace NeoCortexApi.Classifiers
         public int CompareTo(ClassificationAndDistance other) => Distance.CompareTo(other.Distance);
     }
 
-    // KNearestNeighborClassifier class
     public class KNeighborsClassifier<TIN, TOUT> : IClassifier<TIN, TOUT>
     {
-        private int _nNeighbors = 10;
-        private DefaultDictionary<string, List<List<double>>> _models = new DefaultDictionary<string, List<List<double>>>();
+        private DefaultDictionary<string, List<int[]>> _models = new DefaultDictionary<string, List<int[]>>();
         private int _sdrs = 10;
 
-        // Method to calculate the distance between two points
-        private int Distance(List<double> point1, List<double> point2)
+        private int CalculateDistance(int[] sequence1, int[] sequence2)
         {
-            return (int)Math.Sqrt(point1.Select((a, i) => (a - point2[i]) * (a - point2[i])).Sum());
+            // Simple Manhattan distance for simplicity
+            return sequence1.Zip(sequence2, (a, b) => Math.Abs(a - b)).Sum();
         }
 
-        // Method to get the least distance
-        private int LeastValue(ref List<double> classifiedSequence, double unclassifiedPoint)
-        {
-            int shortestDistance = int.MaxValue;
-            int shortestIndex = -1;
-
-            for (int i = 0; i < classifiedSequence.Count; i++)
-            {
-                int distance = Math.Abs((int)(classifiedSequence[i] - unclassifiedPoint));
-                if (distance < shortestDistance)
-                {
-                    shortestDistance = distance;
-                    shortestIndex = i;
-                }
-            }
-
-            return shortestIndex;
-        }
-
-        // Method to calculate distances
-        private Dictionary<int, int> GetDistanceTable(List<double> classifiedSequence, ref List<double> unclassifiedSequence)
+        private Dictionary<int, int> GetDistanceTable(int[] classifiedSequence, int[] unclassifiedSequence)
         {
             var distanceTable = new Dictionary<int, int>();
-
-            foreach (var index in Enumerable.Range(0, unclassifiedSequence.Count))
-                distanceTable[index] = LeastValue(ref classifiedSequence, unclassifiedSequence[index]);
-
+            foreach (var index in unclassifiedSequence)
+                distanceTable[index] = classifiedSequence.Min(classifiedIndex => Math.Abs(classifiedIndex - index));
             return distanceTable;
         }
 
-        // Method to compute distances and classify
-        private List<ClassifierResult<string>> Voting(Dictionary<int, List<ClassificationAndDistance>> mapping, short howMany)
+        private List<ClassifierResult<string>> Voting(Dictionary<int, List<ClassificationAndDistance>> mapping, int howMany)
         {
             var votes = new DefaultDictionary<string, int>();
             var overLaps = new Dictionary<string, int>();
             var similarity = new Dictionary<string, double>();
 
-            // Initializing the overlaps with 0
             foreach (var key in _models.Keys)
                 overLaps[key] = 0;
 
             foreach (var coordinates in mapping)
             {
-                for (int i = 0; i < _nNeighbors; i++)
+                for (int i = 0; i < _models.Count; i++)
                     votes[coordinates.Value[i].Classification] += 1;
 
                 for (int i = 0; i < coordinates.Value.Count; i++)
                 {
-                    if (coordinates.Value[i].Distance.Equals(0))
+                    if (coordinates.Value[i].Distance == 0)
                         overLaps[coordinates.Value[i].Classification] += 1;
                 }
             }
@@ -119,84 +89,64 @@ namespace NeoCortexApi.Classifiers
             var orderedOverLaps = overLaps.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
 
             foreach (var paired in orderedOverLaps)
-            {
-                if (paired.Value != 0)
-                    similarity[paired.Key] = (double)paired.Value / mapping.Count;
-                else
-                    similarity[paired.Key] = 0;
-            }
+                similarity[paired.Key] = paired.Value != 0 ? (double)paired.Value / mapping.Count : 0;
 
             var result = new List<ClassifierResult<string>>();
-            // Checks If the sequence have 50% of overlaps if not the data is ordered in voting manner.
+
             var orderedResults = orderedOverLaps.Values.First() > mapping.Count / 2
                 ? orderedOverLaps.Keys
                 : orderedVotes.Keys;
 
             foreach (var key in orderedResults)
             {
-                var cls = new ClassifierResult<string>();
-                cls.PredictedInput = key;
-                cls.Similarity = similarity[key];
-                cls.NumOfSameBits = overLaps[key];
+                var cls = new ClassifierResult<string>
+                {
+                    PredictedInput = key,
+                    Similarity = similarity[key],
+                    NumOfSameBits = overLaps[key]
+                };
                 result.Add(cls);
             }
 
-            return result.Count > howMany ? result.GetRange(0, howMany) : result;
+            return result.GetRange(0, howMany).ToList();
         }
 
-        // Method to classify an unclassified sequence
         public List<ClassifierResult<TIN>> GetPredictedInputValues(Cell[] unclassifiedCells, short howMany = 1)
         {
             if (unclassifiedCells.Length == 0)
                 return new List<ClassifierResult<TIN>>();
 
-            var unclassifiedSequence = unclassifiedCells.Select(x => x.Index).ToList().ConvertAll(i => (double)i); // Convert to List<double>
+            var unclassifiedSequence = unclassifiedCells.Select(idx => idx.Index).ToArray();
             var mappedElements = new DefaultDictionary<int, List<ClassificationAndDistance>>();
-            _nNeighbors = _models.Values.Count;
 
             foreach (var model in _models)
             {
                 foreach (var (sequence, idx) in model.Value.WithIndex())
                 {
-                    foreach (var dict in GetDistanceTable(sequence, ref unclassifiedSequence))
+                    foreach (var dict in GetDistanceTable(sequence, unclassifiedSequence))
                         mappedElements[dict.Key].Add(new ClassificationAndDistance(model.Key, dict.Value, idx));
                 }
             }
 
             foreach (var mappings in mappedElements)
-                mappings.Value.Sort(); //Sorting values according to distance
+                mappings.Value.Sort();
 
             return Voting(mappedElements, howMany) as List<ClassifierResult<TIN>>;
         }
 
-        private Dictionary<int, int> GetDistanceTable(int[] classifiedSequence, ref List<double> unclassifiedSequence)
-        {
-            var classifiedSequenceList = classifiedSequence.Select(x => (double)x).ToList();
-            var distanceTable = new Dictionary<int, int>();
-
-            foreach (var index in Enumerable.Range(0, unclassifiedSequence.Count))
-            {
-                int indexInt = (int)index;
-                distanceTable[indexInt] = LeastValue(ref classifiedSequenceList, unclassifiedSequence[index]);
-            }
-
-            return distanceTable;
-        }
-        // Method to add new SDRs to the model
         public void Learn(TIN input, Cell[] cells)
         {
             var classification = input as string;
-            var cellIndicies = cells.Select(x => (double)x.Index).ToList(); // Convert Index to double
+            int[] cellIndices = cells.Select(idx => idx.Index).ToArray();
 
-            if (!_models[classification].Exists(seq => seq.SequenceEqual(cellIndicies)))
+            if (!_models[classification].Exists(seq => seq.SequenceEqual(cellIndices)))
             {
                 if (_models[classification].Count > _sdrs)
                     _models[classification].RemoveAt(0);
-                _models[classification].Add(cellIndicies);
+                _models[classification].Add(cellIndices);
             }
         }
 
-        // Method to clear the model
         public void ClearState() => _models.Clear();
     }
 }
