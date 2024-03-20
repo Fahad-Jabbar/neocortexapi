2ffbcb679cd11ff72023fdf4269fdbfcf1a36057
kashif1294 <kashifhu@dlmu.edu.cn>
2024-03-20 16:20:42 +0100

    refactoring

--- source/MySEProject/Task3/KNN_SP_SDRs/KNN_SP_SDRs/KNN_SDRs.cs
+++ source/MySEProject/Task3/KNN_SP_SDRs/KNN_SP_SDRs/KNN_SDRs.cs
@@ -8,22 +8,55 @@ namespace NeoCortexApiSample
     {
         private List<DataPoint> trainingData;
 
-        /// <summary>
-        /// Initializes a new instance of the KNNClassifier class with the given training data.
-        /// </summary>
-        /// <param name="trainingData">The training data to be used for classification.</param>
         public KNNClassifier(List<DataPoint> trainingData)
         {
             this.trainingData = trainingData;
         }
 
-        /// <summary>
-        /// Predicts the label of the given test data point based on the k nearest neighbors in the training data.
-        /// </summary>
-        /// <param name="testDataPoint">The test data point to be classified.</param>
-        /// <param name="k">The number of nearest neighbors to consider.</param>
-        /// <returns>The predicted label of the test data point.</returns>
         public string Predict(DataPoint testDataPoint, int k)
+        {
+            Dictionary<string, int> labelCounts = new Dictionary<string, int>();
+
+            // Maintain a dictionary to store distances and corresponding labels
+            Dictionary<double, string> distanceLabelMap = new Dictionary<double, string>();
+
+            // Calculate distance from testDataPoint to each point in training data
+            foreach (var dataPoint in trainingData)
+            {
+                double distance = CalculateDistance(testDataPoint, dataPoint);
+
+                // Store distance and label in the map
+                distanceLabelMap[distance] = dataPoint.Label;
+            }
+
+            // Sort distances in ascending order
+            var sortedDistances = distanceLabelMap.Keys.OrderBy(x => x);
+
+            // Take the k nearest neighbors
+            int count = 0;
+            foreach (var distance in sortedDistances)
+            {
+                if (count >= k)
+                    break;
+
+                string label = distanceLabelMap[distance];
+
+                // Update label counts
+                if (labelCounts.ContainsKey(label))
+                    labelCounts[label]++;
+                else
+                    labelCounts[label] = 1;
+
+                count++;
+            }
+
+            // Find the label with the highest count
+            string predictedLabel = labelCounts.OrderByDescending(x => x.Value).First().Key;
+
+            return predictedLabel;
+        }
+
+        public List<DistanceLabelPair> CalculateDistances(DataPoint testDataPoint)
         {
             List<DistanceLabelPair> distances = new List<DistanceLabelPair>();
 
@@ -34,35 +67,10 @@ namespace NeoCortexApiSample
                 distances.Add(new DistanceLabelPair(distance, dataPoint.Label));
             }
 
-            // Sort distances in ascending order
-            distances.Sort((x, y) => x.Distance.CompareTo(y.Distance));
-
-            // Take the k nearest neighbors
-            var nearestNeighbors = distances.Take(k);
-
-            // Count the occurrences of each label among the nearest neighbors
-            Dictionary<string, int> labelCounts = new Dictionary<string, int>();
-            foreach (var neighbor in nearestNeighbors)
-            {
-                if (labelCounts.ContainsKey(neighbor.Label))
-                    labelCounts[neighbor.Label]++;
-                else
-                    labelCounts[neighbor.Label] = 1;
-            }
-
-            // Find the label with the highest count
-            string predictedLabel = labelCounts.OrderByDescending(x => x.Value).First().Key;
-
-            return predictedLabel;
+            return distances;
         }
 
-        /// <summary>
-        /// Calculates the Euclidean distance between two data points.
-        /// </summary>
-        /// <param name="point1">The first data point.</param>
-        /// <param name="point2">The second data point.</param>
-        /// <returns>The Euclidean distance between the two data points.</returns>
-        private double CalculateDistance(DataPoint point1, DataPoint point2)
+        public double CalculateDistance(DataPoint point1, DataPoint point2)
         {
             double sumOfSquares = 0;
 
@@ -75,24 +83,16 @@ namespace NeoCortexApiSample
         }
     }
 
-    
     public class DataPoint
     {
-        /// <summary>
-        /// The features of the data point.
-        /// </summary>
         public double[] Features { get; set; }
-
-        /// <summary>
-        /// The label of the data point.
-        /// </summary>
         public string Label { get; set; }
     }
 
     public class DistanceLabelPair
     {
-        public double Distance { get; set; }
-        public string Label { get; set; }
+        public double Distance { get; }
+        public string Label { get; }
 
         public DistanceLabelPair(double distance, string label)
         {
