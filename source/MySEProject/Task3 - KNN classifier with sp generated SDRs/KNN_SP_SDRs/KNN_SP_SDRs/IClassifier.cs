5747d179e136493991a7dd65995787ac57071d5e
kashif1294 <kashifhu@dlmu.edu.cn>
2024-04-08 00:39:58 +0200

    Improved project breakdown for better understanding

---
+++ source/MySEProject/Task3 - KNN classifier with sp generated SDRs/KNN_SP_SDRs/KNN_SP_SDRs/IClassifier.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NeoCortexApi.Entities;
+using System.Collections.Generic;
+using NeoCortexApi.Classifiers;
+
+namespace NeoCortexApiSample
+{
+    /// <summary>
+    /// Interface representing a classifier.
+    /// </summary>
+    public interface IClassifier
+    {
+        /// <summary>
+        /// Calculates distances from a test data point to all points in the training data.
+        /// </summary>
+        /// <param name="testDataPoint">Data point for which distances need to be calculated.</param>
+        /// <returns>List of distance-label pairs.</returns>
+        List<DistanceLabelPair> CalculateDistances(DataPoint testDataPoint);
+
+        /// <summary>
+        /// Predicts the class label for a given test data point using the classifier algorithm.
+        /// </summary>
+        /// <param name="testDataPoint">Data point for which the class label needs to be predicted.</param>
+        /// <param name="k">Number of nearest neighbors to consider.</param>
+        /// <returns>Predicted class label for the test data point.</returns>
+        string Predict(DataPoint testDataPoint, int k);
+    }
+}
